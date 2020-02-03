cde15675732cbbec6df3371ecd537ce457da54fe
Mohammed <faham@amazon.com>
2020-02-03 15:09:48 -0800

    Added layout for formatting log messages in cbor
    Added java doc, addressed minor comments in PR
    
    Fixed import order
    
    Fixed findbugs failures
    
    Added layout for formatting log messages in cbor
    
    Added java doc, addressed minor comments in PR
    
    Fixed import order
    
    Fixed findbugs failures
    
    Added test cases

---
+++ src/test/java/com/aws/iot/evergreen/logging/impl/CBORLayoutTest.java
@@ -0,0 +1,63 @@
+package com.aws.iot.evergreen.logging.impl;
+
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
+import com.fasterxml.jackson.dataformat.cbor.databind.CBORMapper;
+import com.fasterxml.jackson.jr.ob.JSON;
+import com.fasterxml.jackson.jr.stree.JacksonJrsTreeCodec;
+import com.sun.tools.javac.util.List;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.appender.OutputStreamAppender;
+import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+import org.apache.logging.log4j.message.SimpleMessage;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class CBORLayoutTest {
+
+    private static final Layout layout = CBORLayout.createLayout();
+    private static final JSON encoder = JSON.std.with(new JacksonJrsTreeCodec()).with(new CBORFactory());
+    private static final ObjectMapper mapper = new CBORMapper();
+    private static OutputStreamAppender outputStreamAppender;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setupAppender() {
+        outputStreamAppender = OutputStreamAppender.createAppender(layout, null, outContent, "outputStreamAppender", true, false);
+        outputStreamAppender.start();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    @BeforeEach
+    public void resetOutputStream() {
+        outContent.reset();
+    }
+
+    @Test
+    public void logRandomThings() throws IOException {
+
+        List<SimpleMessage> messages = List.of(new SimpleMessage("message 1"), new SimpleMessage("message 2"), new SimpleMessage("msg 3"));
+        messages.forEach((m) -> {
+            Log4jLogEvent log4jLogEvent = new Log4jLogEvent.Builder().setMessage(m).build();
+            outputStreamAppender.append(log4jLogEvent);
+        });
+
+        ByteBuffer buf = ByteBuffer.wrap(outContent.toByteArray());
+        for (SimpleMessage message : messages) {
+            int length = buf.getInt();
+            byte[] arr = new byte[length];
+            buf.get(arr);
+            TreeNode tree = encoder.treeFrom(arr);
+            SimpleMessage deserializedMessage = tree.traverse(mapper).readValueAs(SimpleMessage.class);
+            //comparing the original message with the de-serialized message
+            assert deserializedMessage.equals(message);
+        }
+    }
+}
