e7802e9584dd96de2c7ce60efa22275693375cab
Mohammed <faham@amazon.com>
2020-02-03 16:30:37 -0800

    Added java doc, addressed minor comments in PR

--- src/main/java/com/aws/iot/evergreen/logging/impl/CBORLayout.java
+++ src/main/java/com/aws/iot/evergreen/logging/impl/CBORLayout.java
@@ -2,10 +2,10 @@ package com.aws.iot.evergreen.logging.impl;
 
 import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 import com.fasterxml.jackson.jr.ob.JSON;
-import com.fasterxml.jackson.jr.stree.JacksonJrsTreeCodec;
 import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.config.Node;
+import org.apache.logging.log4j.message.Message;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 import org.apache.logging.log4j.core.layout.AbstractLayout;
@@ -13,16 +13,19 @@ import org.apache.logging.log4j.core.layout.AbstractLayout;
 import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
 
-
+/** Serialises the {@link Message} in {@link LogEvent} to CBOR format
+ *
+ *  For each {@link Message} the layout reserves the initial four bytes to store the length of the serialized message.
+ */
 @Plugin(name = "CBORLayout", category = Node.CATEGORY, elementType = Layout.ELEMENT_TYPE, printObject = false)
 public class CBORLayout extends AbstractLayout<LogEvent> {
 
-    private static final JSON encoder = JSON.std.with(new JacksonJrsTreeCodec()).with(new CBORFactory());
+    private static final JSON encoder = JSON.std.with(new CBORFactory());
     private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
     private static final int NUM_OF_BYTES_REPRESENTING_MESSAGE_LENGTH = 4;
     private static final byte[] ARRAY_REPRESENTING_MESSAGE_LENGTH = new byte[NUM_OF_BYTES_REPRESENTING_MESSAGE_LENGTH];
 
-    private CBORLayout(){
+    private CBORLayout() {
         super(null, null, null);
     }
 
@@ -32,11 +35,10 @@ public class CBORLayout extends AbstractLayout<LogEvent> {
     }
 
     /**
-     *
      * Encodes the message in the log event using cbor data format
      *
      * @param event
-     * @return  First 4 bytes represent the size of the serialized message followed
+     * @return First 4 bytes represent the size of the serialized message followed
      * by message itself.
      */
     @Override
@@ -50,7 +52,7 @@ public class CBORLayout extends AbstractLayout<LogEvent> {
             // wrapping the bytes array in a buffer to write the size of the serialized payload
             // in the first four bytes
             ByteBuffer buffer = ByteBuffer.wrap(eventInBytes);
-            buffer.putInt(0,eventInBytes.length - NUM_OF_BYTES_REPRESENTING_MESSAGE_LENGTH);
+            buffer.putInt(0, eventInBytes.length - NUM_OF_BYTES_REPRESENTING_MESSAGE_LENGTH);
             return buffer.array();
         } catch (Exception e) {
             LOGGER.error("Serialization of LogEvent failed.", e);
