99ac4499f52101f86b6fa641de614047fe69e726
Hui Yang <huiyn@amazon.com>
2020-03-05 09:45:55 -0800

    Update copyrights

--- src/main/java/com/aws/iot/evergreen/ipc/IPCClientImpl.java
+++ src/main/java/com/aws/iot/evergreen/ipc/IPCClientImpl.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.iot.evergreen.ipc;
 
 import com.aws.iot.evergreen.ipc.codec.MessageFrameDecoder;
