f1acc82ed5169da1de59e029cf4fb94eb51ae64f
Michael Dombrowski <mdombro@amazon.com>
2023-12-01 14:25:01 -0500

    fix: update ExecutorServiceUtil for logback 1.3.12

--- src/main/java/ch/qos/logback/core/util/ExecutorServiceUtil.java
+++ src/main/java/ch/qos/logback/core/util/ExecutorServiceUtil.java
@@ -7,6 +7,8 @@ package ch.qos.logback.core.util;
 
 import ch.qos.logback.core.CoreConstants;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -26,10 +28,35 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class ExecutorServiceUtil {
 
-    private static final ThreadFactory THREAD_FACTORY = new ThreadFactory() {
+    static private final String NEW_VIRTUAL_TPT_METHOD_NAME = "newVirtualThreadPerTaskExecutor";
 
-        private final ThreadFactory defaultFactory = Executors.defaultThreadFactory();
+    private static final ThreadFactory THREAD_FACTORY_FOR_SCHEDULED_EXECUTION_SERVICE = new ThreadFactory() {
+
         private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+
+        private final ThreadFactory defaultFactory = makeThreadFactory();
+
+        /**
+         * A thread factory which may be a virtual thread factory the JDK supports it.
+         *
+         * @return
+         */
+        private ThreadFactory makeThreadFactory() {
+            if (EnvUtil.isJDK21OrHigher()) {
+                try {
+                    Method ofVirtualMethod = Thread.class.getMethod("ofVirtual");
+                    Object threadBuilderOfVirtual = ofVirtualMethod.invoke(null);
+                    Method factoryMethod = threadBuilderOfVirtual.getClass().getMethod("factory");
+                    return (ThreadFactory) factoryMethod.invoke(threadBuilderOfVirtual);
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                    return Executors.defaultThreadFactory();
+                }
+
+            } else {
+                return Executors.defaultThreadFactory();
+            }
+        }
 
         @Override
         public Thread newThread(Runnable r) {
@@ -46,6 +73,7 @@ public class ExecutorServiceUtil {
 
     /**
      * Creates a scheduled executor service suitable for use by logback components.
+     *
      * @return scheduled executor service
      */
     public static synchronized ScheduledExecutorService newScheduledExecutorService() {
@@ -54,36 +82,59 @@ public class ExecutorServiceUtil {
         // 2) Cache and return the same executor so that there's a single threadpool in use to decrease
         //  the number of threads which Logback creates.
         if (executor == null || executor.isShutdown() || executor.isTerminated() || executor.isTerminating()) {
-            executor = new ScheduledThreadPoolExecutor(1, THREAD_FACTORY);
+            executor = new ScheduledThreadPoolExecutor(1, THREAD_FACTORY_FOR_SCHEDULED_EXECUTION_SERVICE);
         }
         return executor;
     }
 
     /**
      * Creates threadpool executor suitable for use by logback components.
+     *
      * @return threadpool executor
      */
-    public static ThreadPoolExecutor newThreadPoolExecutor() {
+    static public ThreadPoolExecutor newThreadPoolExecutor() {
         return new ThreadPoolExecutor(CoreConstants.CORE_POOL_SIZE, CoreConstants.MAX_POOL_SIZE, 0L,
-                TimeUnit.MILLISECONDS, new SynchronousQueue<>(), THREAD_FACTORY);
+                TimeUnit.MILLISECONDS, new SynchronousQueue<>(), THREAD_FACTORY_FOR_SCHEDULED_EXECUTION_SERVICE);
     }
 
 
     /**
      * Creates an executor service suitable for use by logback components.
+     *
      * @deprecated replaced by {@link #newThreadPoolExecutor()}
      */
-    public static ExecutorService newExecutorService() {
+    static public ExecutorService newExecutorService() {
         return newThreadPoolExecutor();
     }
 
     /**
      * Shuts down an executor service.
+     * <p>
+     *
      * @param executorService the executor service to shut down
      */
     public static void shutdown(ExecutorService executorService) {
         if (executorService != null) {
             executorService.shutdownNow();
+        }
+    }
+
+    /**
+     * An alternate implementation of {@link #newThreadPoolExecutor} which returns a virtual thread per task executor
+     * when available.
+     *
+     * @since 1.3.12/1.4.12
+     */
+    static public ExecutorService newAlternateThreadPoolExecutor() {
+        if (EnvUtil.isJDK21OrHigher()) {
+            try {
+                Method newVirtualTPTMethod = Executors.class.getMethod(NEW_VIRTUAL_TPT_METHOD_NAME);
+                return (ExecutorService) newVirtualTPTMethod.invoke(null);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                return newThreadPoolExecutor();
+            }
+        } else {
+            return newThreadPoolExecutor();
         }
     }
 }
